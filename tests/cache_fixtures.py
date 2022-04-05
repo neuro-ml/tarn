3912dcc3e3adca070a87b6e0ddb4414c049c50d0
Max <maxs987@gmail.com>
2022-04-06 00:32:56 +0300

    fixed hashes

--- tests/cache_fixtures.py
+++ tests/cache_fixtures.py
@@ -10,7 +10,7 @@ REFERENCES = {
         (1, functions.identity): '9b8b1e3ae950963e609f8164ba76403c6790731db5e1cbbdb56a0cf9cd886005',
         (1, functions.nested_identity): '9e23d47084c40f37054da37a345e26fd85bbf5ad733d84f790acb03d8e16d115',
         (1, classes.One): '4acfbb8cc1dfb43db698ebedc9c6eb0b23d71b2dc021e2f684cfa321b8d5aa01',
-        (1, classes.A): 'afef34c32cf2c8e94350c2bf02d57d8a1f7cdf5f0a81a180892276011f6594cd',
+        (1, classes.A): '47de846b6392330138bde757e463d6f420496a007ec7407c5933678b3a0f0c89',
     }
 }
 
