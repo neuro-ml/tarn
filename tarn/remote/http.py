448f4afde20fbcad9dbf43d0666906ad433d3067
Max <maxs987@gmail.com>
2023-04-08 16:29:12 +0400

    legacy "optional" argument

--- tarn/remote/http.py
+++ tarn/remote/http.py
@@ -1,3 +1,6 @@
 from ..location.nginx import Nginx
 
-HTTPLocation = Nginx
+
+class HTTPLocation(Nginx):
+    def __init__(self, url: str, optional: bool = False):
+        super().__init__(url)
--- tarn/remote/ssh.py
+++ tarn/remote/ssh.py
@@ -1,3 +1,9 @@
+from paramiko.config import SSH_PORT
+
 from ..location.scp import SCP
 
-SSHLocation = SCP
+
+class SSHLocation(SCP):
+    def __init__(self, hostname: str, root, port: int = SSH_PORT, username: str = None, password: str = None,
+                 key=(), optional: bool = False):
+        super().__init__(hostname, root, port, username, password, key)
