f4af8b1a7cd28ec3baebfcca4673917a84ace94e
Talgat <saparov.tn@phystech.edu>
2023-06-14 15:35:59 +0300

    update test_s3

--- tests/test_locations/test_s3.py
+++ tests/test_locations/test_s3.py
@@ -18,13 +18,9 @@ STORAGE_ROOT = 'test'
 def test_storage_ssh():
     s3 = boto3.resource('s3', endpoint_url='http://127.0.0.1:8001', aws_access_key_id='admin', aws_secret_access_key='SeCrEtKeYlOCaL98327498732')
     bucket = 'kekis'
-    config = StorageConfig(hash='blake2b', levels=[1, 63])
-    s3.Object('kekis', f'{STORAGE_ROOT}/config.yml').put(Body=config.json())
-    s3.Bucket(bucket).put_object(Key=f'{STORAGE_ROOT}/config.yml', Body=config.json())
     location = S3(s3, bucket, STORAGE_ROOT)
     storage = HashKeyStorage(location)
     key = storage.write(__file__)
-    # raise ValueError(key, type(key))
     file = storage.read(lambda x: x, key)
     with pytest.raises(ReadError):
         file = storage.read(lambda x: x, b'keke' * 16)
