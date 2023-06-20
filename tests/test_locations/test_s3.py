ed17ab28319c9c1a54959939208ae28a33590921
Talgat <saparov.tn@phystech.edu>
2023-06-20 22:03:33 +0300

    update and skip s3 tests

--- tests/test_locations/test_s3.py
+++ tests/test_locations/test_s3.py
@@ -2,26 +2,17 @@ import boto3
 import pytest
 
 from tarn import ReadError, HashKeyStorage
-from tarn.config import StorageConfig
 from tarn.location.s3 import S3
 
 
-def load_text(path):
-    with open(path, 'r') as file:
-        return file.read()
-
-
-STORAGE_ROOT = 'test'
-
-
-@pytest.mark.s3
-def test_storage_ssh():
-    s3 = boto3.resource('s3', endpoint_url='http://127.0.0.1:8001', aws_access_key_id='admin', aws_secret_access_key='SeCrEtKeYlOCaL98327498732')
+@pytest.mark.skip(reason='not ready yet')
+def test_storage_s3():
+    s3 = boto3.client('s3', endpoint_url='http://127.0.0.1:8001', aws_access_key_id='admin', aws_secret_access_key='SeCrEtKeYlOCaL98327498732')
     bucket = 'kekis'
-    location = S3(s3, bucket, STORAGE_ROOT)
+    location = S3(s3, bucket)
     storage = HashKeyStorage(location)
-    key = storage.write(__file__)
+    key = storage.write(__file__, labels=('IRA', 'LABS'))
     file = storage.read(lambda x: x, key)
     with pytest.raises(ReadError):
-        file = storage.read(lambda x: x, b'keke' * 16)
+        file = storage.read(lambda x: x, b'keke' * 8)
     location.delete(key)
