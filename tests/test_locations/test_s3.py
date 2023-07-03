b694c6b3140c7f7f78651ace9eb8b49d2931c4d9
talgat <t.saparov@ira-labs.com>
2023-07-03 19:53:38 +0300

    trying to setup minio

--- tests/test_locations/test_s3.py
+++ tests/test_locations/test_s3.py
@@ -7,24 +7,27 @@ from tarn import HashKeyStorage, ReadError
 from tarn.location.s3 import S3
 
 
-@pytest.mark.skip('not ready yet')
-def test_storage_s3():
-    s3 = boto3.client('s3', endpoint_url='http://10.0.1.2:11354')
+def test_storage_s3(inside_ci):
+    if inside_ci:
+        s3 = boto3.client('s3', endpoint_url='http://127.0.0.1:8001', aws_access_key_id='admin', aws_secret_access_key='adminadminadminadmin')
+    else:
+        s3 = boto3.client('s3', endpoint_url='http://10.0.1.2:11354')
     bucket = 'test'
     location = S3(s3, bucket)
     storage = HashKeyStorage(location, algorithm=blake2b)
     key = storage.write(__file__, labels=('IRA', 'LABS'))
+    key = storage.write(__file__, labels=('IRA', 'LABS', 'IRA1'))
     file = storage.read(lambda x: x, key)
     with location.write(b'123/456', b'123456', None) as v:
         pass
     with location.write(b'123/4567', __file__, None) as v:
         pass
     with location.read(key, return_labels=True) as content:
-        assert sorted(content[1]) == sorted(['IRA', 'LABS'])
+        assert sorted(content[1]) == sorted(['IRA', 'LABS', 'IRA1'])
     with pytest.raises(ReadError):
         file = storage.read(lambda x: x, b'keke' * 8)
     keys_amount = len(list(location.contents()))
     location.delete(key)
     location.delete(b'123/456')
     location.delete(b'123/4567')
-    assert keys_amount - len(list(location.contents())) == 2
+    assert keys_amount - len(list(location.contents())) == 3
