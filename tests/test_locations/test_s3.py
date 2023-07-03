751de6e5fb925470255d012f61dad18368173c42
talgat <t.saparov@ira-labs.com>
2023-07-03 18:19:32 +0300

    and tests

--- tests/test_locations/test_s3.py
+++ tests/test_locations/test_s3.py
@@ -1,8 +1,9 @@
 from hashlib import blake2b
+
 import boto3
 import pytest
 
-from tarn import ReadError, HashKeyStorage
+from tarn import HashKeyStorage, ReadError
 from tarn.location.s3 import S3
 
 
@@ -14,13 +15,16 @@ def test_storage_s3():
     storage = HashKeyStorage(location, algorithm=blake2b)
     key = storage.write(__file__, labels=('IRA', 'LABS'))
     file = storage.read(lambda x: x, key)
-    with location.write('123/456', b'123456', None) as v:
+    with location.write(b'123/456', b'123456', None) as v:
         pass
-    with location.write('123/4567', __file__, None) as v:
+    with location.write(b'123/4567', __file__, None) as v:
         pass
     with location.read(key, return_labels=True) as content:
-        assert content[1] == ['IRA', 'LABS']
+        assert sorted(content[1]) == sorted(['IRA', 'LABS'])
     with pytest.raises(ReadError):
         file = storage.read(lambda x: x, b'keke' * 8)
-    location.contents()
+    keys_amount = len(list(location.contents()))
     location.delete(key)
+    location.delete(b'123/456')
+    location.delete(b'123/4567')
+    assert keys_amount - len(list(location.contents())) == 2
