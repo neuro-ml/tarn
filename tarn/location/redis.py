2347d1d9ff62ca7a8cc061fdf1d4c91f273bb6ac
talgat <t.saparov@ira-labs.com>
2023-07-13 14:46:03 +0300

    return single quotes back

--- tarn/location/redis.py
+++ tarn/location/redis.py
@@ -12,16 +12,16 @@ from .interface import Writable
 
 
 class RedisLocation(Writable):
-    def __init__(self, redis: Union[Redis, str], prefix: str = ""):
+    def __init__(self, redis: Union[Redis, str], prefix: str = ''):
         if isinstance(redis, str):
             redis = Redis.from_url(redis)
         self.redis = redis
-        self.prefix = prefix.encode("utf-8")
+        self.prefix = prefix.encode('utf-8')
         self.hash = None
         self.key_size = None
 
     def contents(self) -> Iterable[Tuple[Key, Any, Meta]]:
-        for raw_key in self.redis.scan_iter(match=self.prefix + b"*"):
+        for raw_key in self.redis.scan_iter(match=self.prefix + b'*'):
             key = raw_key[len(self.prefix):]
             yield key, self, RedisMeta(key=key, location=self)
 
@@ -67,7 +67,7 @@ class RedisLocation(Writable):
                 old_content = self.redis.get(content_key)
                 if old_content != value.read():
                     raise CollisionError(
-                        f"Written value and the new one doesn't match: {key}"
+                        f'Written value and the new one doesn't match: {key}'
                     )
                 self.update_labels(key, labels)
                 self.update_usage_date(key)
@@ -77,33 +77,33 @@ class RedisLocation(Writable):
             self.delete(key)
 
     def get_labels(self, key: Key) -> MaybeLabels:
-        labels_key = b"labels" + self.prefix + key
+        labels_key = b'labels' + self.prefix + key
         labels_bytes = self.redis.get(labels_key)
         if labels_bytes is None:
             return
         return list(json.loads(labels_bytes))
 
     def update_labels(self, key: Key, labels: MaybeLabels):
-        labels_key = b"labels" + self.prefix + key
+        labels_key = b'labels' + self.prefix + key
         old_labels = self.get_labels(key) or []
         if labels is not None:
             labels = list(set(old_labels).union(labels))
             self.redis.set(labels_key, json.dumps(labels))
 
     def get_usage_date(self, key: Key) -> Optional[datetime]:
-        usage_date_key = b"usage_date" + self.prefix + key
+        usage_date_key = b'usage_date' + self.prefix + key
         usage_date = self.redis.get(usage_date_key)
         if usage_date is not None:
             return datetime.fromtimestamp(float(usage_date))
 
     def update_usage_date(self, key: Key):
-        usage_date_key = b"usage_date" + self.prefix + key
+        usage_date_key = b'usage_date' + self.prefix + key
         self.redis.set(usage_date_key, datetime.now().timestamp())
 
     def delete(self, key: Key):
         content_key = self.prefix + key
-        labels_key = b"labels" + self.prefix + key
-        usage_date_key = b"usage_date" + self.prefix + key
+        labels_key = b'labels' + self.prefix + key
+        usage_date_key = b'usage_date' + self.prefix + key
         self.redis.delete(content_key, labels_key, usage_date_key)
 
 
@@ -120,4 +120,4 @@ class RedisMeta(Meta):
         return self._location.get_labels(self._key)
 
     def __str__(self):
-        return f"{self.last_used}, {self.labels}"
+        return f'{self.last_used}, {self.labels}'
--- tarn/location/s3.py
+++ tarn/location/s3.py
@@ -30,12 +30,12 @@ class S3(Writable):
         self.key_size = None
 
     def contents(self) -> Iterable[Tuple[Key, Any, Meta]]:
-        paginator = self.s3.get_paginator("list_objects_v2")
+        paginator = self.s3.get_paginator('list_objects_v2')
         response_iterator = paginator.paginate(Bucket=self.bucket)
         for response in response_iterator:
-            if "Contents" in response:
-                for obj in response["Contents"]:
-                    path = obj["Key"]
+            if 'Contents' in response:
+                for obj in response['Contents']:
+                    path = obj['Key']
                     key = self._path_to_key(path)
                     yield key, self, S3Meta(path=path, location=self)
 
@@ -55,8 +55,8 @@ class S3(Writable):
 
             except ClientError as e:
                 if (
-                    e.response["Error"]["Code"] == "404"
-                    or e.response["Error"]["Code"] == "NoSuchKey"
+                    e.response['Error']['Code'] == '404'
+                    or e.response['Error']['Code'] == 'NoSuchKey'
                 ):  # file doesn't exist
                     yield
                 else:
@@ -82,7 +82,7 @@ class S3(Writable):
                         match_buffers(value, obj_body_buffer, context=key.hex())
                     except ValueError as e:
                         raise CollisionError(
-                            f"Written value and the new one doesn't match: {key}"
+                            f'Written value and the new one doesn't match: {key}'
                         ) from e
                     self.update_labels(path, labels)
                     self.update_usage_date(path)
@@ -90,8 +90,8 @@ class S3(Writable):
                     return
                 except ClientError as e:
                     if (
-                        e.response["Error"]["Code"] == "404"
-                        or e.response["Error"]["Code"] == "NoSuchKey"
+                        e.response['Error']['Code'] == '404'
+                        or e.response['Error']['Code'] == 'NoSuchKey'
                     ):
                         self.s3.upload_fileobj(
                             Bucket=self.bucket, Key=path, Fileobj=value
@@ -114,36 +114,36 @@ class S3(Writable):
     def update_labels(self, path: str, labels: MaybeLabels):
         if labels is not None:
             tags_dict = self._tags_to_dict(
-                self.s3.get_object_tagging(Bucket=self.bucket, Key=path)["TagSet"]
+                self.s3.get_object_tagging(Bucket=self.bucket, Key=path)['TagSet']
             )
-            tags_dict.update({f"_{label}": f"_{label}" for label in labels})
+            tags_dict.update({f'_{label}': f'_{label}' for label in labels})
             tags = self._dict_to_tags(tags_dict)
             self.s3.put_object_tagging(
-                Bucket=self.bucket, Key=path, Tagging={"TagSet": tags}
+                Bucket=self.bucket, Key=path, Tagging={'TagSet': tags}
             )
 
     def get_labels(self, path: str) -> MaybeLabels:
         tags_dict = self._tags_to_dict(
-            self.s3.get_object_tagging(Bucket=self.bucket, Key=path)["TagSet"]
+            self.s3.get_object_tagging(Bucket=self.bucket, Key=path)['TagSet']
         )
-        return [dict_key[1:] for dict_key in tags_dict if dict_key[0] == "_"]
+        return [dict_key[1:] for dict_key in tags_dict if dict_key[0] == '_']
 
     def update_usage_date(self, path: str):
         tags_dict = self._tags_to_dict(
-            self.s3.get_object_tagging(Bucket=self.bucket, Key=path)["TagSet"]
+            self.s3.get_object_tagging(Bucket=self.bucket, Key=path)['TagSet']
         )
-        tags_dict["usage_date"] = str(datetime.now().timestamp())
+        tags_dict['usage_date'] = str(datetime.now().timestamp())
         tags = self._dict_to_tags(tags_dict)
         self.s3.put_object_tagging(
-            Bucket=self.bucket, Key=path, Tagging={"TagSet": tags}
+            Bucket=self.bucket, Key=path, Tagging={'TagSet': tags}
         )
 
     def get_usage_date(self, path: str) -> Optional[datetime]:
         tags_dict = self._tags_to_dict(
-            self.s3.get_object_tagging(Bucket=self.bucket, Key=path)["TagSet"]
+            self.s3.get_object_tagging(Bucket=self.bucket, Key=path)['TagSet']
         )
-        if "usage_date" in tags_dict:
-            return datetime.fromtimestamp(float(tags_dict["usage_date"]))
+        if 'usage_date' in tags_dict:
+            return datetime.fromtimestamp(float(tags_dict['usage_date']))
         return None
 
     def _get_buffer(self, path):
@@ -153,29 +153,29 @@ class S3(Writable):
         return str(key_to_relative(key, [2, len(key) - 2]))
 
     def _path_to_key(self, path: str):
-        return bytes.fromhex(path.replace("/", ""))
+        return bytes.fromhex(path.replace('/', ''))
 
     @staticmethod
     def _tags_to_dict(tags: Iterable[Mapping[str, str]]) -> Mapping[str, str]:
-        return {tag["Key"]: tag["Value"] for tag in tags}
+        return {tag['Key']: tag['Value'] for tag in tags}
 
     @staticmethod
     def _dict_to_tags(tag_dict: Mapping[str, str]) -> Iterable[Mapping[str, str]]:
-        return [{"Key": key, "Value": value} for key, value in tag_dict.items()]
+        return [{'Key': key, 'Value': value} for key, value in tag_dict.items()]
 
 
 class StreamingBodyBuffer(BinaryIO):
     def __init__(self, getter, **kwargs):
         super().__init__()
         self.getter, self.kwargs = getter, kwargs
-        self._streaming_body = getter(**kwargs).get("Body")
+        self._streaming_body = getter(**kwargs).get('Body')
 
     def seek(self, offset: int, whence: int = SEEK_SET) -> int:
         # we can either return to the begining of the stream or do nothing
         #  everythnig else is too expensive
         if whence == SEEK_SET:
             if offset == 0:
-                self._streaming_body = self.getter(**self.kwargs).get("Body")
+                self._streaming_body = self.getter(**self.kwargs).get('Body')
                 return
             if offset == self.tell():
                 return
@@ -184,19 +184,19 @@ class StreamingBodyBuffer(BinaryIO):
             if offset == 0:
                 return
             if offset == -self.tell():
-                self._streaming_body = self.getter(**self.kwargs).get("Body")
+                self._streaming_body = self.getter(**self.kwargs).get('Body')
                 return
 
         if whence == SEEK_END:
             if offset == 0:
                 return
 
-        raise NotImplementedError("Can't seek anywhere but the begining of the stream")
+        raise NotImplementedError('Can't seek anywhere but the begining of the stream')
 
     def __getattribute__(self, attr) -> Any:
-        if attr in ("seek", "getter", "kwargs"):
+        if attr in ('seek', 'getter', 'kwargs'):
             return super().__getattribute__(attr)
-        streaming_body = super().__getattribute__("_streaming_body")
+        streaming_body = super().__getattribute__('_streaming_body')
         return getattr(streaming_body, attr)
 
 
@@ -213,4 +213,4 @@ class S3Meta(Meta):
         return self._location.get_labels(self._path)
 
     def __str__(self):
-        return f"{self.last_used}, {self.labels}"
+        return f'{self.last_used}, {self.labels}'
