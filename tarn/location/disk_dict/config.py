2aff07255a2ba39f59b912c759b37965e774f902
talgat <t.saparov@ira-labs.com>
2024-01-26 18:08:14 +0300

    try default_factory

--- tarn/location/disk_dict/config.py
+++ tarn/location/disk_dict/config.py
@@ -52,13 +52,7 @@ class HashConfig(NoExtra):
 class ToolConfig(NoExtra):
     name: str
     args: Tuple = ()
-    kwargs: Optional[Dict[str, Any]] = Field(None, validate_default=True)
-
-    @field_validator('kwargs', always=True)
-    def normalize_kwargs(cls, v):
-        if v is None:
-            return {}
-        return v
+    kwargs: Optional[Dict[str, Any]] = Field(None, default_factory=dict)
 
 
 class StorageConfig(NoExtra):
